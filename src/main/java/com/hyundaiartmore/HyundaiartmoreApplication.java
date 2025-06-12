c6583575eab7f5e0acdc58dd5a5c3d0ae07b763b
Seohyunjun <seohyunjun@Seohyunjunui-MacBookAir.local>
2025-06-12 18:43:15 +0900

    HyundaiArtmore

---
+++ src/main/java/com/hyundaiartmore/HyundaiartmoreApplication.java
@@ -0,0 +1,13 @@
+package com.hyundaiartmore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyundaiartmoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyundaiartmoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyundaiartmore/HyundaiartmoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyundaiartmore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyundaiartmoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
